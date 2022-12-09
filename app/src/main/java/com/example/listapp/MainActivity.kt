78a279c5427af4aa6e8d4657e57446a9bc78495f
yueshanwang <yueshan314@gmail.com>
2022-12-09 03:16:55 -0500

    initial commit

---
+++ app/src/main/java/com/example/listapp/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.example.listapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import org.json.JSONArray
+import java.io.IOException
+import java.net.URL
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerview: RecyclerView
+
+    //sort by list id, then name
+    class listData(val listID: Int, val name: String, val id: Int) : Comparable<listData> {
+        override fun compareTo(other: listData) = compareValuesBy(this, other, { it.listID }, { it.name })
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val fetch = fetch()
+        fetch.start()
+        fetch.join()//wait till the JSON has finished downloading
+        val data = fetch.data
+        val jArray = JSONArray(data)
+        val list = ArrayList<listData>()
+
+        for (i in 0 until jArray.length()) {
+
+            val jsonObject = jArray.getJSONObject(i)
+
+            //not blank and not null name
+            if(!jsonObject.get("name").equals("") && !jsonObject.get("name").equals(null)){
+                val toAdd = listData(jsonObject.get("listId") as Int, jsonObject.get("name") as String, jsonObject.get("id") as Int)
+                list.add(toAdd)
+            }
+        }
+        //send sorted list to adapter
+        val adapter = MyAdapter(list.sorted())
+        this.recyclerview = findViewById(R.id.fetchList)
+        this.recyclerview.layoutManager = LinearLayoutManager(this)
+        this.recyclerview.adapter = adapter
+    }
+
+    class fetch: Thread() {
+
+        var data: String = ""
+
+        override fun run(){
+            try{
+                this.data  = URL("https://fetch-hiring.s3.amazonaws.com/hiring.json").readText()
+            } catch(e: IOException){
+                e.printStackTrace()
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/listapp/MyAdapter.kt
@@ -0,0 +1,36 @@
+package com.example.listapp
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class MyAdapter internal constructor(private val data: List<MainActivity.listData>): RecyclerView.Adapter<MyAdapter.ViewHolder>() {
+
+    class ViewHolder(view: View): RecyclerView.ViewHolder(view){
+
+        val leftView: TextView = view.findViewById(R.id.textView)
+        val middleView: TextView = view.findViewById(R.id.textView2)
+        val rightView: TextView = view.findViewById(R.id.textView3)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_view, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val obj = data[position]
+
+        holder.leftView.text = obj.listID.toString()
+        holder.middleView.text = obj.name
+        holder.rightView.text = obj.id.toString()
+    }
+}
